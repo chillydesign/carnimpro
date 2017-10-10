e68bcc18caf80e187bf6f476aa58b49906b59c03
Charles Harvey <harvey.charles@gmail.com>
2017-10-10 08:53:08 +0100

    fix js to make forms button show nice options properly

--- js/scripts.js
+++ js/scripts.js
@@ -7,22 +7,44 @@ import underscore from '../node_modules/underscore/underscore.js'
         'use strict';
 
 
+        $(document).on('keydown', function(e){
+            if(e.keyCode == 27 ){
+                // if pressing escape
+            }
+        }).on('click', function(e){
+            // hide nicerselects when click on rest of window, except for button
+            $('.nicer_select').removeClass('visible');
+        })
+
+
+
+
+        // HEADER SLIDE NAVIGATION
+        $('nav a').on('click', function(e){
+            e.preventDefault();
+            var $id = $(this).attr('href');
+            var $page_location = $($id);
+            $("html, body").animate({ scrollTop: $page_location.offset().top }, 1000);
+        })
+
+
+
 
         var $form_buttons = $('.form_button');
         $form_buttons.each(function(i){
             var $button = $(this);
 
             var $checkboxes = $('input[type="checkbox"]', $button);
-
             var $span = $('span', $button);
-
             var $nicer_select = $('.nicer_select', $button);
 
             $button.on('click', function(e){
-
+                e.stopPropagation();
                 $nicer_select.toggleClass('visible');
+                $('.nicer_select').not($nicer_select).removeClass('visible');  // hide others
 
-            });
+
+            }); // end of when you click on a form button
 
 
             var $nicer_options = $('.nicer_option', $nicer_select);
@@ -47,12 +69,9 @@ import underscore from '../node_modules/underscore/underscore.js'
                     };
                 }
 
-            })
+            }) // end of when you click on a nice option
 
-
-
-
-        });
+        });  // end of for each form_buttons
 
 
 
